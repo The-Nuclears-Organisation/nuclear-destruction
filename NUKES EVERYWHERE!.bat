19041ddab45a6381fdc7b353951abe1c503ca02a
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2021-09-21 12:27:10 +0530

    1.1

--- NUKES EVERYWHERE!.bat
+++ NUKES EVERYWHERE!.bat
@@ -4,10 +4,11 @@ title NUKES!!!
 goto start
 :start
 cls
-echo 1.LAUNCH A NUKE 2.STAY CALM 
+echo 1.LAUNCH A NUKE 2.STAY CALM 3.KILL UR ENEMIES
 set /p input= SELECT FAST!:
 if %input% == 1 goto launch
-if %input% == 1 goto calm
+if %input% == 2 goto calm
+if %input% == 3 goto
 :launch
 cls
 echo you killed the whole world (you incl.) :(
@@ -17,5 +18,10 @@ goto start
 cls
 echo although everybody is ded, you survived! :) but no mcdonalds and no Internet :(
 echo score: 50-ish
+pause
+goto start
+:enemy_kill
+cls
+echo oh! you killed your enemies and you won! although your casualties were %random%
 pause
 goto start
