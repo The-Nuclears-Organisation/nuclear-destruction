22cd1d7a5e7a8cceffa5587da142cdf548a63d54
FrankEnderman <90335574+FrankEnderman@users.noreply.github.com>
2021-09-24 13:05:15 +0530

    added new options

--- NUKES EVERYWHERE!.bat
+++ NUKES EVERYWHERE!.bat
@@ -1,27 +1,45 @@
 @echo off
 color 0a 
-title NUKES!!!
+title NUKES EVERYWHERE!!!
+set score == 0
 goto start
+
 :start
 cls
-echo 1.LAUNCH A NUKE 2.STAY CALM 3.KILL UR ENEMIES
+echo 1.LAUNCH A NUKE 2.STAY CALM 3.KILL UR ENEMIES 4.S*icide 5.exit
+echo score: %score%
 set /p input= SELECT FAST!:
 if %input% == 1 goto launch
 if %input% == 2 goto calm
-if %input% == 3 goto
+if %input% == 3 goto enemy_kill
+if %input% == 4 goto ded
+if %input% == 5 goto exit
 :launch
 cls
 echo you killed the whole world (you incl.) :(
+SET /A score = %score% - 1
 pause 
 goto start
 :calm
 cls
 echo although everybody is ded, you survived! :) but no mcdonalds and no Internet :(
 echo score: 50-ish
+SET /A score = %score% + 1
 pause
 goto start
+
 :enemy_kill
 cls
 echo oh! you killed your enemies and you won! although your casualties were %random%
+SET /A score = %score% + 1
 pause
 goto start
+:ded
+cls
+echo You Died! 
+SET /A score = %score% - 2
+pause
+goto start
+:exit
+echo %score% > score.txt
+exit
